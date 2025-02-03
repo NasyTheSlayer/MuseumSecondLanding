378b49c4673870795155af6d14579ce0a7293cce
NasyTheSlayer <nazar.levkovich.2018@gmail.com>
2025-02-03 15:25:48 +0200

    make build

---
+++ index.93446de6.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")});var e=document.getElementById("form"),n=document.getElementById("user-email");e.addEventListener("submit",function(e){e.preventDefault(),n.value=""});
+//# sourceMappingURL=index.93446de6.js.map
